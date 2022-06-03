48b36352fddcd7a2636b30f52ca8e49d655f39db
Sebastian <hvber01@gmail.com>
2022-06-03 18:56:11 +0200

    Add context

---
+++ src/context/Context.ts
@@ -0,0 +1,25 @@
+import { createContext } from "react";
+export interface IProduct{
+    categories: number[],
+    description: string,
+    ean: number,
+    id: number,
+    image: string,
+    images: Object[],
+    name: string,
+    net_price: number,
+    price: string,
+    tags: string[],
+    taxes: number,
+    upc: number
+}
+interface IShopContext {
+    products: IProduct[],
+    favouriteProducts: IProduct[],
+    setFavouriteProducts: any
+}
+export const ShopContext = createContext<IShopContext>({
+    products: [],
+    favouriteProducts: [],
+    setFavouriteProducts: ()=>{}
+})
