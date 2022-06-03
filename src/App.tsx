a8d91c90c092fb3dfe497cf985c776fcd0a317cd
Sebastian <hvber01@gmail.com>
2022-06-03 18:56:43 +0200

    Add favourite products

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,39 @@
 import Nav from './components/organisms/nav/Nav';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import Home from './components/organisms/home/Home';
+import { ShopContext } from './context/Context';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [products, setProducts] = useState<[]>([]);
+  const [favouriteProducts, setFavouriteProducts] = useState<[]>([]);
+  useEffect(()=>{
+    if(!products.length){
+      axios.get('https://fakerapi.it/api/v1/products?_price_min=5&_price_max=1000', {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }).then(res => {
+        setProducts(res.data.data)
+      })
+    }
+  }, [products.length])
   return (
-    <Router>
-      <Nav />
-      <Routes>
-        
-      </Routes>
-    </Router>
+    <ShopContext.Provider value={{
+      products, 
+      favouriteProducts, 
+      setFavouriteProducts
+    }}>
+      <Router>
+        <Nav />
+        <main>
+          <Routes>
+              <Route path='/' element={<Home />} />
+          </Routes>
+        </main>
+      </Router>
+    </ShopContext.Provider>
   );
 }
 
