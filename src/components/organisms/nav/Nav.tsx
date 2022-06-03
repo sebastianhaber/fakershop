05ad174c9900be1d919eb274a2dce3075ec26299
Sebastian <hvber01@gmail.com>
2022-06-03 16:36:09 +0200

    Add navigation

--- src/components/organisms/nav/Nav.tsx
+++ src/components/organisms/nav/Nav.tsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 export default function Nav() {
     return (
         <StyledNav>
-            <div id="logo">Shop</div>
+            <Link to='/' id='logo'>Shop.</Link>
             <div className="box">
                 <Link to='/favourites'><Icon icon="akar-icons:heart" /></Link>
                 <Link to='/cart'><Icon icon="akar-icons:basket" /></Link>
@@ -15,12 +15,39 @@ export default function Nav() {
     )
 }
 const StyledNav = styled.nav`
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 1;
+    background-color: rgba(255,255,255,.9);
     display: flex;
     align-items: center;
+    justify-content: space-between;
+    width: 100%;
     height: 70px;
     padding: 0 2rem;
+    font-size: 1.5rem;
     #logo{
         font-weight: bold;
-        font-size: 1.5rem;
+        text-transform: uppercase;
+        letter-spacing: -3px;
+    }
+    .box{
+        display: flex;
+        align-items: center;
+        gap: 1rem;
+        a{
+            display: grid;
+            place-content: center;
+            padding: .5rem;
+            &:hover{
+                color: ${({theme}) => theme.colors.primary};
+            }
+        }
+    }
+    @media screen and (min-width: 500px) {
+        .box{
+            gap: 2rem;
+        }
     }
 `;
