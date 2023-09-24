a09c032a77340814b0776ed49944c1dc55f977a7
Sebastian Haber <hvber01@gmail.com>
2023-09-24 13:00:25 +0200

    Display searchBar only in Homepage

--- src/components/organisms/nav/Nav.tsx
+++ src/components/organisms/nav/Nav.tsx
@@ -1,6 +1,6 @@
 import { Icon } from "@iconify/react";
 import { useContext, useMemo } from "react";
-import { Link, useSearchParams } from "react-router-dom";
+import { Link, useLocation, useSearchParams } from "react-router-dom";
 import { ShopContext } from "../../../context/Context";
 import { StyledNav } from "./Nav.styled";
 
@@ -8,6 +8,8 @@ const Nav = () => {
   const { favouriteProducts, cart } = useContext(ShopContext);
   const [searchParams, setSearchParams] = useSearchParams();
   const query = useMemo(() => searchParams.get("q") || "", [searchParams]);
+  const location = useLocation();
+  const isHomePage = useMemo(() => location.pathname === "/", [location]);
 
   const handleSearch = (value: string) => {
     if (!value) searchParams.delete("q");
@@ -24,14 +26,17 @@ const Nav = () => {
         {/* Shop. */}
         BIO - SHOP.
       </Link>
-      <input
-        type="search"
-        placeholder="Search product..."
-        className="search"
-        onChange={(e) => handleSearch(e.target.value)}
-        defaultValue={query}
-        autoFocus={query.length > 0}
-      />
+
+      {isHomePage && (
+        <input
+          type="search"
+          placeholder="Search product..."
+          className="search"
+          onChange={(e) => handleSearch(e.target.value)}
+          defaultValue={query}
+          autoFocus={query.length > 0}
+        />
+      )}
       <div className="box">
         <Link to="/favourites" id="favs">
           <Icon icon="akar-icons:heart" />
